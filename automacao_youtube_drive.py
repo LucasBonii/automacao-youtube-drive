97f6a602c0cc433909d4bb26d690003ef208a7ee
LucasBonii <158228644+LucasBonii@users.noreply.github.com>
2025-08-14 15:47:21 -0300

    começando integração com a api do google drive

--- automacao_youtube_drive.py
+++ automacao_youtube_drive.py
@@ -1,3 +1,9 @@
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import yt_dlp
 
 def baixar_video(link):
@@ -11,6 +17,19 @@ def baixar_video(link):
     
     return ydl_opts['outtmpl']
 
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
-arquivo = baixar_video("https://www.youtube.com/watch?v=DfcXJYv_dxE")
-print("Vídeo baixado:", arquivo)
+def autenticar_drive():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    service = build('drive', 'v3', credentials=creds)
+    return service
+
+
+service= autenticar_drive()
